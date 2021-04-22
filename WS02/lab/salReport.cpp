c6f64b7f8f163878fa5b22a1e6c61ab1a21bcd4d
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-21 23:22:27 -0300

    Update salReport.cpp

--- WS02/lab/salReport.cpp
+++ WS02/lab/salReport.cpp
@@ -1,11 +1,3 @@
-// Workshop 2: 
-// Version: 0.9
-// Date: 2021/1/23
-// Author: Fardad Soleimanloo
-// Description:
-// This file tests the lab section of your workshop
-// Do not modify your code
-/////////////////////////////////////////////
 #include "Employee.h"
 
 using namespace sdds;
