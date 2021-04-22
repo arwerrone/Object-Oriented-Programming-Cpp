3ea26328794b268eaf05b3f335c26874b30123b1
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-21 23:20:04 -0300

    Update popReport.cpp

--- WS02/DIY/popReport.cpp
+++ WS02/DIY/popReport.cpp
@@ -1,11 +1,3 @@
-// Workshop 2: 
-// Version: 0.9
-// Date: 2021/01/23
-// Author: Fardad Soleimanloo
-// Description:
-// This file tests the DIY section of your workshop
-// Do not modify your code
-/////////////////////////////////////////////
 #include "Population.h"
 using namespace sdds;
 int main() {
