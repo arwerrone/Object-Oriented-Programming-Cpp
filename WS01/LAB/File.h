c065ba9417ec4f9b4dca2d72416f8e820ec84681
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-21 23:14:15 -0300

    Update File.h

--- WS01/LAB/File.h
+++ WS01/LAB/File.h
@@ -1,9 +1,3 @@
-/**
- * Name: Angelo Dikon Wong
- * Student ID:148021199
- * Seneca email: adikon-wong@myseneca.ca
- * Section: OOP244 - NGG
- **/
 #ifndef NAMESPACE_FILE_H // replace with relevant names
 #define NAMESPACE_FILE_H
 #include "ShoppingRec.h"
