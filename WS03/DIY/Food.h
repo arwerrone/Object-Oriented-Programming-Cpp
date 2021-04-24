1c00d29e76c3f89dbf6243343bc90cf665e41663
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-23 22:28:45 -0300

    Update Food.h

--- WS03/DIY/Food.h
+++ WS03/DIY/Food.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #ifndef SDDS_FOOD_H_
 #define SDDS_FOOD_H_
 namespace sdds {
@@ -24,4 +17,4 @@ namespace sdds {
     };
 }
 
-#endif // !SDDS_SUBJECT_H
+#endif
