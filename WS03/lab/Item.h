6dd619f4f2079b13d5e2196d85852ec646ecc6ee
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-23 22:32:06 -0300

    Update Item.h

--- WS03/lab/Item.h
+++ WS03/lab/Item.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #ifndef SDDS_ITEM_H_
 #define SDDS_ITEM_H_
 namespace sdds {
@@ -23,4 +16,4 @@ namespace sdds {
    };
 }
 
-#endif // !SDDS_SUBJECT_H
+#endif
