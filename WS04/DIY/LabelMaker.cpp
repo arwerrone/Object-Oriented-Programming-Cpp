f1453089122ec87e5f1f706d1e46a78bbb47db13
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-23 22:41:07 -0300

    Update LabelMaker.cpp

--- WS04/DIY/LabelMaker.cpp
+++ WS04/DIY/LabelMaker.cpp
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		148021199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section: OOP244 - NGG
- **/
-
 #include <iostream>
 #include "LabelMaker.h"
 
