f2cd7b5ef7cf02e6fcc349c937d1613a7c79a65c
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-23 22:40:43 -0300

    Update LabelMaker.h

--- WS04/DIY/LabelMaker.h
+++ WS04/DIY/LabelMaker.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		148021199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section: OOP244 - NGG
- **/
-
 #ifndef SDDS_LABELMAKER_H
 #define SDDS_LABELMAKER_H
 #include <iostream>
