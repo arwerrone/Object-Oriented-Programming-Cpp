385f45b5d36693a76c216a970749200a026b492e
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-24 22:10:48 -0300

    Update Mark.h

--- WS05/DIY/Mark.h
+++ WS05/DIY/Mark.h
@@ -1,10 +1,3 @@
-/**
- * Name:                Angelo Dikon Wong
- * Student ID:          148021199
- * Seneca email:        adikon-wong@myseneca.ca
- * Section:             OOP244 - NGG
- **/
-
 #ifndef SDDS_MARK_H_
 #define SDDS_MARK_H_
 #include <iostream>
