8ee8a835196a2eac3c78ec43926abebdf890b0c0
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-24 22:19:49 -0300

    Update cstring.h

--- WS06/lab/cstring.h
+++ WS06/lab/cstring.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #ifndef SDDS_CSTRING_H_
 #define SDDS_CSTRING_H_
 namespace sdds {
@@ -12,7 +5,4 @@ namespace sdds {
    int strLen(const char* s);
    void strCat(char* des, const char* src);
 }
-#endif // !SDDS_CSTRING_H_
-
-
-
+#endif
