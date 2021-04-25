e655e395bfea69f6e22d8e181400655cf1c21c9c
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-24 22:52:37 -0300

    Update Line.h

--- WS08/lab/Line.h
+++ WS08/lab/Line.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:			148021199
- * Seneca email:		adikon-wong@myseneca.ca
- * Section: OOP244 - NGG
- **/
-
 #ifndef SDDS_LINE_H__
 #define SDDS_LINE_H__
 #include <iostream>
