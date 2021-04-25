e491dd9b807e24870df6151262694105d6d976d1
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-24 22:32:57 -0300

    Update Truck.h

--- WS07/Truck.h
+++ WS07/Truck.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #ifndef SDDS_TRUCK_H__
 #define SDDS_TRUCK_H__
 #include <iostream>
