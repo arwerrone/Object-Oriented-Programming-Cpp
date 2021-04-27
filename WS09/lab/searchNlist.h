bb92a7a7d712083aa13ea48bc5094feebd8d8917
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-27 20:54:58 -0300

    Update searchNlist.h

--- WS09/lab/searchNlist.h
+++ WS09/lab/searchNlist.h
@@ -1,23 +1,9 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:			148021199
- * Seneca email:		adikon-wong@myseneca.ca
- * Section: OOP244 - NGG
- **/
-
 #ifndef SDDS_SEARCHNLIST_H_
 #define SDDS_SEARCHNLIST_H_
 #include "Collection.h"
 #include <iostream>
 using namespace std;
 namespace sdds {
-	
-	//For this template function work, we have to create two typename,
-	//one is used to the array that we are reading the values,
-	//the other one is the key, the value that we are using to compare.
-	//We use different typename, because it may vary.
-	//On the first parameter we add the Collection template because it will
-	//be responsible for storing the data.
 	template<typename T, typename T2>
 	bool search(Collection<T>& OMG, const T* arra, int numb, T2 key) {
 		bool ok = false;
@@ -30,10 +16,6 @@ namespace sdds {
 		return ok;
 	}
 
-	//For this template function, the inclusion of only one typename is enought,
-	//this typename is used as the array that we are going to print.
-	//comparing to the past template that we created, this does not require another
-	//template as a parameter, the reason is that this functon will only print.
 	template<typename T>
 	void listArrayElements(const char* tit, const T arra, int numb) {
 		cout << tit << endl;
@@ -42,7 +24,6 @@ namespace sdds {
 			cout << arra[i] << endl;
 		}
 	}
-
 }
 
 #endif
