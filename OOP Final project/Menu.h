e7c1ca3c6316a57daca5fbf3e9fff5d431b47149
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-27 21:16:51 -0300

    Update Menu.h

--- OOP Final project/Menu.h
+++ OOP Final project/Menu.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #ifndef SDDS_MENU_H_
 #define SDDS_MENU_H_
 #include <iostream>
