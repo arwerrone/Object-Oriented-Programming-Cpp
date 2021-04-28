30be94b9f3e3a98ffe48b634f55e20a2db6bd5db
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-27 21:17:28 -0300

    Update CovidPatient.h

--- OOP Final project/CovidPatient.h
+++ OOP Final project/CovidPatient.h
@@ -1,11 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
-
 #ifndef SDDS_COVIDPATIENT_H_
 #define SDDS_COVIDPATIENT_H_
 #include "Patient.h"
@@ -21,7 +13,4 @@ namespace sdds {
 	};
 
 }
-#endif // !SDDS_COVIDPATIENT_H_
-
-
-
+#endif
