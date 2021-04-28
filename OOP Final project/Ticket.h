de5dce97f0a01db0fbe430aa15e34d0a173f572a
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-27 21:15:11 -0300

    Update Ticket.h

--- OOP Final project/Ticket.h
+++ OOP Final project/Ticket.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #ifndef SDDS_TICKET_H_
 #define SDDS_TICKET_H_
 #include "Time.h"
@@ -24,4 +17,4 @@ namespace sdds {
       std::istream& read(std::istream& istr);
    };
 }
-#endif // !SDDS_TICKET_H_
+#endif
