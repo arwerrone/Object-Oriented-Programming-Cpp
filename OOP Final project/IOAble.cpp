3441f9e5b879c1daeeee69982956a9300e1edfb3
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-27 21:17:16 -0300

    Update IOAble.cpp

--- OOP Final project/IOAble.cpp
+++ OOP Final project/IOAble.cpp
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #include <iostream>
 #include "IOAble.h"
 
