8bc964bc981f41bbfcfe0cd3d1b272a1ecec73a3
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-27 21:15:02 -0300

    Update Time.cpp

--- OOP Final project/Time.cpp
+++ OOP Final project/Time.cpp
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #include "Time.h"
 #include "utils.h"
 using namespace std;
