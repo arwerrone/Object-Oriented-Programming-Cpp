a36ad2bfff69a1e65c86d7b8f2335433fd8fd380
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2021-04-27 21:14:13 -0300

    Update TriagePatient.h

--- OOP Final project/TriagePatient.h
+++ OOP Final project/TriagePatient.h
@@ -1,10 +1,3 @@
-/**
- * Name:			Angelo Dikon Wong
- * Student ID:		14802199
- * Seneca email:	adikon-wong@myseneca.ca
- * Section:			OOP244 - NGG
- **/
-
 #ifndef SDDS_TRIAGEPATIENT_H
 #define SDDS_TRIAGEPATIENT_H
 #include "Patient.h"
@@ -20,12 +13,9 @@ namespace sdds {
 		std::ostream& write(std::ostream& ostr)const;
 		std::istream& read(std::istream& istr);
 		~TriagePatient();
-		//my implemented fucntion
-		void exterminate();	//this function will delete the dynamic memory. Reference to doctor who
-		void angelo(std::istream& istr); //this function will dynamically alocate cstring to symp. No creative name this time
+		void exterminate();
+		void angelo(std::istream& istr);
 	};
 
 }
-#endif // !SDDS_TRIAGEPATIENT_H
-
-
+#endif
